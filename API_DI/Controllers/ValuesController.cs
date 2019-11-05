dfb50d6c3feef09815e7dd4d36bc99429aae57ac
Manuel Maldonado <manuel-alejandro.maldonado-rojas@mfa.gouv.qc.ca>
2019-11-05 09:55:15 -0500

    Premier_commit

--- API_DI/Controllers/ValuesController.cs
+++ API_DI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace API_DI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2","value preuve" };
         }
 
         // GET api/values/5
